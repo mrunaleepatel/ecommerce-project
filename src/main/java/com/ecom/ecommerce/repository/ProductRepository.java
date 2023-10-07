b6a8bd9837c8dcf968f2f77d59e02a6cbbd223d3
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-10-07 13:59:32 -0500

    product

--- src/main/java/com/ecom/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/ecom/ecommerce/repository/ProductRepository.java
@@ -1,4 +1,19 @@
 package com.ecom.ecommerce.repository;
 
-public class ProductRepository {
+import com.ecom.ecommerce.entity.Product;
+
+import java.util.List;
+
+public interface ProductRepository {
+    Product get(int productId);
+    List<Product>list();
+    boolean add(Product product);
+    boolean update(Product product);
+    boolean delete(Product product);
+    List<Product>getProductsByParam(String param, int count);
+    List<Product>listActiveProducts();
+    List<Product>listActiveProductsByCategory(int categoryId);
+    List<Product>getLatestActiveProducts(int count);
+
+
 }
